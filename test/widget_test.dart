564c1a20fd924b8e2b82bba388a6f73851fb3db5
Sahrulproject <sahrulhakim15@gmail.com>
2025-09-06 03:50:19 +0700

    fix merge conflict

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,6 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-<<<<<<< HEAD
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
@@ -29,28 +27,3 @@
 //     expect(find.text('1'), findsOneWidget);
 //   });
 // }
-=======
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tixclick/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
->>>>>>> 993bf42bcb5066326c76ab82adb26fdf54e1c9e5
